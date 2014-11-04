f07c040537c7ed12b34f07685bed8b86a6122f30
kclaxto <k_claxton@yahoo.com>
2014-11-03 17:19:25 -0800

    Mighty Minerals
    Mohs Hardness Scale Game

--- PhlappyBirds/game.js
+++ PhlappyBirds/game.js
@@ -7,25 +7,52 @@ var main = {
         game.stage.backgroundColor = '#71c5cf';	
         
         game.load.image('player','diamond.png');
-        game.load.image('gem1','topaz.png');
+        game.load.image('topaz','topaz.png');
+        game.load.image('calcite','calcite.png');
+        game.load.image('Corundum','Corundum.png');
+        game.load.image('Feldspar','Feldspar.png');
+        game.load.image('fluorite','fluorite.png');
+        game.load.image('Gypsum','Gypsum.png');
+        game.load.image('Quartz','Quartz.png');
+        game.load.image('Talc','Talc.png');
+        game.load.image('Apatite','Apatite.png');
+        
     },
 
     // set up the game
     create: function() {
         
-        game.physics.startSystem(Phaser.Physics.ARCARDE);
         
         // create the player using an image and place it at (100, 245)
-        this.player = game.add.sprite(300, 160, 'player');
-        this.gem1 = game.add.sprite(300, 300, 'gem1');
+        this.player = game.add.sprite(0, 5, 'player');
+        this.topaz =  game.add.sprite(100, 15, 'topaz');
+        this.player = game.add.sprite(180, 15, 'calcite');
+        this.player = game.add.sprite(240, 15, 'Corundum');
+        this.player = game.add.sprite(350, 15, 'Feldspar');
+        this.player = game.add.sprite(450, 15, 'fluorite');
+        this.player = game.add.sprite(500, 15, 'Gypsum');
+        this.player = game.add.sprite(600, 15, 'Quartz');
+        this.player = game.add.sprite(700, 15, 'Talc');
+        this.player = game.add.sprite(800, 15, 'Apatite');
         
+        this.player.width = 90;
+        this.player.height = 90;
+        this.topaz.width = 90;
+        this.topaz.height = 90;
+        this.Corundum.width = 90;
+        this.Corundum.height = 90;
+        this.Feldspar.width = 90;
+        this.Feldspar.height = 90;
+        this.Gypsum.width = 90;
+        this.Gypsum.height = 90;
+        this.quartz.width = 90;
+        this.quartz.height = 90;
+        this.Talc.width = 90;
+        this.Talc.height = 90;
+        this.Apatite.width = 10;
+        this.Apatite.height = 10;
         
-        
-game.physics.arcade.enable(this.player);
-        
-game.physics.arcade.enable(this.gem1);
-    
-this.player.body.gravity.y=1000;
+        game.physics.arcade.enable(this.player);
         
         var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
@@ -38,16 +65,23 @@ this.player.body.gravity.y=1000;
         if (this.player.inWorld==false)
             this.restartGame();
         
-        if (game.physics.arcade.overlap(this.player,this.gem1)) {
-          this.gem1.body.velocity.y=700;  
+        if (game.physics.arcade.overlap(this.player,this.topaz)) {
+
         }
     },
   
     // makes the player jump
     jump: function() {
-  this.player.body.velocity.y=-500;
         
-  this.gem1.body.velocity.y=-700;
+     var emitter = game.add.emitter(game.world.centerX, 200, 200);
+
+    emitter.makeParticles('player');
+
+    //	false means don't explode all the sprites at once, but instead release at a rate of 20 particles per frame
+    //	The 5000 value is the lifespan of each particle
+    emitter.start(false, 5000, 20);
+   
+  this.gem1.body.velocity.x=-700;
         
     },
   
